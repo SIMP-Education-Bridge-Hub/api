d89b6b8a41b10b06b6af4dd70109b5008ae3af40
JP Tiyasi <theanthem8@gmail.com>
2025-03-16 19:56:47 +0000

    Create auth controller

---
+++ src/controllers/v1/auth/index.js
@@ -0,0 +1,70 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { PrismaClient } from "@prisma/client";
+import dotenv from "dotenv";
+
+dotenv.config();
+const prisma = new PrismaClient();
+const SECRET_KEY = process.env.JWT_SECRET;
+
+// User Registration
+export const registerUser = async (req, res) => {
+  try {
+    const { name, surname, email, password, phone } = req.body;
+
+    // Check if user exists
+    const existingUser = await prisma.user.findUnique({ where: { email } });
+
+    if (existingUser)
+      return res.status(400).json({ message: "User already exists" });
+
+    // Hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create user
+    const user = await prisma.user.create({
+      data: {
+        name,
+        surname,
+        email,
+        password: hashedPassword,
+        phone: "0794194768",
+        userTypeId: {
+          connect: { userTypeId: "5bd7727d-6fcf-49f0-bce0-d7ff546ea6e9" },
+        },
+      },
+    });
+
+    res.status(201).json({ message: "User registered successfully", user });
+  } catch (error) {
+    res.status(500).json({ message: "Error registering user", error });
+  }
+};
+
+// User Login
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Check if user exists
+    const user = await prisma.user.findUnique({ where: { email } });
+    if (!user) return res.status(400).json({ message: "Invalid credentials" });
+
+    // Check password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch)
+      return res.status(400).json({ message: "Invalid credentials" });
+
+    // Generate JWT token
+    const token = jwt.sign({ userId: user.id }, SECRET_KEY, {
+      expiresIn: "1h",
+    });
+
+    res.json({
+      token,
+      user: { id: user.id, name: user.name, email: user.email },
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Error logging in", error });
+  }
+};
