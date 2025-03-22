9cd4580c8f7ea55d31b0a5f1bce8fc3ab543713b
JP Tiyasi <theanthem8@gmail.com>
2025-03-22 17:36:51 +0000

    Separate login and register control

---
+++ src/controllers/v1/auth/login/index.js
@@ -0,0 +1,36 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { PrismaClient } from "@prisma/client";
+import dotenv from "dotenv";
+
+dotenv.config();
+const prisma = new PrismaClient();
+const SECRET_KEY = process.env.JWT_SECRET;
+
+// User Login
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Check if user exists
+    const user = await prisma.user.findUnique({ where: { email } });
+    if (!user) return res.status(400).json({ message: "Invalid credentials" });
+
+    // Check password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch)
+      return res.status(400).json({ message: "Invalid credentials" });
+
+    // Generate JWT token
+    const token = jwt.sign({ userId: user.id }, SECRET_KEY, {
+      expiresIn: "1h",
+    });
+
+    res.json({
+      token,
+      user: { id: user.id, name: user.name, email: user.email },
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Error logging in", error });
+  }
+};
--- src/controllers/v1/auth/register/index.js
+++ src/controllers/v1/auth/register/index.js
@@ -8,7 +8,7 @@ const prisma = new PrismaClient();
 const SECRET_KEY = process.env.JWT_SECRET;
 
 // User Registration
-export const registerUser = async (req, res) => {
+export const register = async (req, res) => {
   try {
     const { name, surname, email, password, phone } = req.body;
 
@@ -40,33 +40,5 @@ export const registerUser = async (req, res) => {
     console.log("Error", error);
 
     res.status(500).json({ message: "Error registering user", error });
-  }
-};
-
-// User Login
-export const loginUser = async (req, res) => {
-  try {
-    const { email, password } = req.body;
-
-    // Check if user exists
-    const user = await prisma.user.findUnique({ where: { email } });
-    if (!user) return res.status(400).json({ message: "Invalid credentials" });
-
-    // Check password
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch)
-      return res.status(400).json({ message: "Invalid credentials" });
-
-    // Generate JWT token
-    const token = jwt.sign({ userId: user.id }, SECRET_KEY, {
-      expiresIn: "1h",
-    });
-
-    res.json({
-      token,
-      user: { id: user.id, name: user.name, email: user.email },
-    });
-  } catch (error) {
-    res.status(500).json({ message: "Error logging in", error });
   }
 };
