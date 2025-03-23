2472ab7ceb2b83a5e93cb64fedf37480a8fdceca
JP Tiyasi <theanthem8@gmail.com>
2025-03-23 11:04:08 +0000

    Add register middleware

--- src/routes/v1/auth/index.js
+++ src/routes/v1/auth/index.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { registerUser, loginUser } from "../../../controllers/v1/auth/index.js";
+import { register } from "../../../controllers/v1/auth/register/index.js";
+import { login } from "../../../controllers/v1/auth/login/index.js";
 import { loginMiddleware } from "../../../middleware/v1/auth/login/index.js";
+import { registerMiddleware } from "../../../middleware/v1/auth/register/index.js";
 
 const router = express.Router();
 
-router.post("/register", registerUser);
-router.post("/login", loginMiddleware, loginUser);
+router.post("/register", registerMiddleware, register);
+router.post("/login", loginMiddleware, login);
 
 export default router;
