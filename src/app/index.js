b021173b6c18fc44676f52df058cd3dfe7db9fea
JP Tiyasi <theanthem8@gmail.com>
2025-03-23 11:01:08 +0000

    Add roles and seed database endpoints

--- src/app/index.js
+++ src/app/index.js
@@ -3,6 +3,10 @@ import cors from "cors";
 import authRoutes from "../routes/v1/auth/index.js";
 import userRoutes from "../routes/v1/user/index.js";
 import jwtRoute from "../routes/v1/jwt/index.js";
+import seedDatabaseRoute from "../routes/v1/seed-database/index.js";
+import rolesRoutes from "../routes/v1/roles/index.js";
+
+import { createUser } from "../database/faker/users/index.js";
 
 const app = express();
 
@@ -11,12 +15,12 @@ app.use(express.json());
 
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/users", userRoutes);
+app.use("/api/v1/roles", rolesRoutes);
 
 app.use("/api/v1/jwt", jwtRoute);
+app.use("/api/v1/seed-database", seedDatabaseRoute);
 
 app.get("/", (req, res) => {
-  // res.send("Welcome to the REST API");
   res.send("Welcome to SIMP restful API");
 });
-
 export default app;
