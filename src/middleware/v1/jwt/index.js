69ac8ee6988bafca84e73d328cb121f61da737b8
JP Tiyasi <theanthem8@gmail.com>
2025-03-23 11:02:34 +0000

    Add jwt validation

--- src/middleware/v1/jwt/index.js
+++ src/middleware/v1/jwt/index.js
@@ -1,3 +1,4 @@
+import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 import { JWT_VALIDATION_SCHEMA } from "../../../utils/v1/validations/auth/jwt/index.js";
 
@@ -6,11 +7,18 @@ const SECRET_KEY = process.env.JWT_SECRET;
 
 export const jwtMiddleware = async (req, res, next) => {
   try {
+    const token = req.header("Authorization")?.split(" ")[1];
+    if (!token) return res.status(401).json({ message: "Access Denied" });
+    console.log("token", token);
+    const verified = jwt.verify(token, SECRET_KEY, {
+      audience: "/api/v1/jwt",
+    });
+    console.log("verified", verified);
     await JWT_VALIDATION_SCHEMA.validate(req.body, { abortEarly: false });
     next();
   } catch (error) {
     return res.status(400).json({
-      message: "Validation Error",
+      message: error?.message ?? error,
       errors: error.errors,
     });
   }
