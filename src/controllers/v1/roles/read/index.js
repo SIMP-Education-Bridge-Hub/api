178c1ad0d4c29351437a481dca7ff4cf87da6ff4
JP Tiyasi <theanthem8@gmail.com>
2025-03-29 04:34:54 +0000

    Create read single role control function

--- src/controllers/v1/roles/read/index.js
+++ src/controllers/v1/roles/read/index.js
@@ -1,13 +1,16 @@
 import { PrismaClient } from "@prisma/client";
 import dotenv from "dotenv";
-import { roleCollection } from "../../../../utils/v1/api/resources/roles/index.js";
+import {
+  roleCollection,
+  roleResource,
+} from "../../../../utils/v1/api/resources/roles/index.js";
 
 dotenv.config();
 const prisma = new PrismaClient();
 const SECRET_KEY = process.env.JWT_SECRET;
 
 // User Registration
-export const readRole = async (req, res) => {
+export const readRoles = async (req, res) => {
   // No need for body checks. Middleware handles it all
   try {
     const {} = req.body;
@@ -19,5 +22,24 @@ export const readRole = async (req, res) => {
     console.log("Error", error);
 
     res.status(500).json({ message: "Error registering user", error });
+  }
+};
+
+export const readRole = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const role = await prisma.role.findUnique({
+      where: { roleId: id },
+    });
+
+    if (!role)
+      return res.status(404).json({ message: `Role with id ${id} not found` });
+
+    res.status(200).json(roleResource(role));
+  } catch (error) {
+    console.log("Error", error);
+
+    res.status(500).json({ message: error?.message ?? error });
   }
 };
