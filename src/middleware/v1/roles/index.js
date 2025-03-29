1b3d0424ba49b6867dd82e472681eea7c86d0b4c
JP Tiyasi <theanthem8@gmail.com>
2025-03-29 04:30:29 +0000

    Create read single role middleware

--- src/middleware/v1/roles/index.js
+++ src/middleware/v1/roles/index.js
@@ -1,6 +1,9 @@
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
-import { CREATE_ROLE_VALIDATION_SCHEMA } from "../../../utils/v1/validations/auth/role/index.js";
+import {
+  CREATE_ROLE_VALIDATION_SCHEMA,
+  VIEW_ROLE_VALIDATION_SCHEMA,
+} from "../../../utils/v1/validations/auth/role/index.js";
 
 dotenv.config();
 const SECRET_KEY = process.env.JWT_SECRET;
@@ -17,11 +20,38 @@ export const rolesMiddleware = async (req, res, next) => {
 
     console.log("Method", req.method);
 
-    if (req.method === "POST") {
+    if (req.method.toLowerCase() === "post") {
+      console.log("Validate");
+
       await CREATE_ROLE_VALIDATION_SCHEMA.validate(req.body, {
         abortEarly: false,
       });
     }
+
+    next();
+  } catch (error) {
+    return res.status(400).json({
+      message: error?.message ?? error,
+      errors: error.errors,
+    });
+  }
+};
+
+export const roleMiddleware = async (req, res, next) => {
+  try {
+    const token = req.header("Authorization")?.split(" ")[1];
+    if (!token) return res.status(401).json({ message: "Access Denied" });
+    console.log("token", token);
+    const verified = jwt.verify(token, SECRET_KEY, {
+      audience: "/api/v1/roles",
+    });
+    console.log("verified", verified);
+
+    console.log("Method", req.method);
+
+    await VIEW_ROLE_VALIDATION_SCHEMA.validate(req.params, {
+      abortEarly: false,
+    });
 
     next();
   } catch (error) {
