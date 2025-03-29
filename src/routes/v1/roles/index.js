dbb6cbf939e22e66d71a1a01939d9a4935d9558b
JP Tiyasi <theanthem8@gmail.com>
2025-03-29 04:27:21 +0000

    Create read single role route

--- src/routes/v1/roles/index.js
+++ src/routes/v1/roles/index.js
@@ -1,12 +1,20 @@
 import express from "express";
 import { createRole } from "../../../controllers/v1/roles/create/index.js";
-import { readRole } from "../../../controllers/v1/roles/read/index.js";
-import { rolesMiddleware } from "../../../middleware/v1/roles/index.js";
+import {
+  readRoles,
+  readRole,
+} from "../../../controllers/v1/roles/read/index.js";
+import {
+  roleMiddleware,
+  rolesMiddleware,
+} from "../../../middleware/v1/roles/index.js";
 
 const router = express.Router();
 
 router.post("/", rolesMiddleware, createRole);
 
-router.get("/", rolesMiddleware, readRole);
+router.get("/", rolesMiddleware, readRoles);
+
+router.get("/:id", rolesMiddleware, roleMiddleware, readRole);
 
 export default router;
